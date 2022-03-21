86d0f78ec464b5dfb05bbf056c1e4889f42ed2b3
iriosarroyo <94561622+iriosarroyo@users.noreply.github.com>
2022-03-21 18:13:50 +0100

    Added click and keydown events

--- script.js
+++ script.js
@@ -1,15 +1,33 @@
 {
-  //TODO: Get new password fields added after page loaded
-  const createEye = (field) =>{
-    const wrapper = document.createElement("div");
-    wrapper.classList.add("viewPassword-1234-qwerty-password-wrapper")
-    const parentElem = field.parentElement;
-    parentElem.append(wrapper)
-    wrapper.append(field)
+  const WRAPPER_CLASS = "viewPassword-1234-qwerty-password-wrapper";
+  const togglePassword = (field) => {
+    field.type = field.type === "password" ? "text" : "password";
   }
   
+  const handleClickWrapper = (event, field) =>{
+    if(event.target.classList.contains(WRAPPER_CLASS)) togglePassword(field);
+  }
+  
+  const createEye = (field) =>{
+    const wrapper = document.createElement("div");
+    wrapper.classList.add(WRAPPER_CLASS);
+    const parentElem = field.parentElement;
+    parentElem.append(wrapper);
+    wrapper.append(field);
+    wrapper.addEventListener("click", (event) => handleClickWrapper(event, field));
+  }
+  
+  //TODO: Get new password fields added after page loaded
   const passwordFields = document.querySelectorAll("input[type=password]");
-  passwordFields.forEach(field =>{
-    createEye(field);
+  passwordFields.forEach(createEye);
+  
+  document.addEventListener("keydown", (event) =>{
+    if(!event.ctrlKey || !event.shiftKey) return;
+    let fieldIdx = parseInt(String.fromCharCode(event.keyCode));
+    if(Number.isNaN(fieldIdx)) return;
+    if(fieldIdx === 9) fieldIdx = passwordFields.length
+    if(fieldIdx > passwordFields.length) return;
+    const field = passwordFields[fieldIdx - 1];
+    togglePassword(field);
   })
 }
